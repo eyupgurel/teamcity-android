fefb3bf3871c3fc7eef581511e8a793f3ee2ebed
Eyüp Gürel <eyupgurel@gmail.com>
2018-02-27 15:38:30 +0300

    D

--- dock-teamcity-on-the-bridge.sh
+++ dock-teamcity-on-the-bridge.sh
@@ -33,7 +33,7 @@ eyupgurel/teamcity-android-agent:2017.2.2 bash /run-services.sh
 
 
 docker container run -d --name tmcdb -v ~/mount/postgres/teamcity/:/var/lib/postgresql/data \
---network bridge -p 5432:5432 \
+--network bridge -p 5433:5432 \
 -e POSTGRES_PASSWORD=eX933dISQSLT \
 -e shared_buffers=512MB \
 -e max_wal_size=1500MB \
